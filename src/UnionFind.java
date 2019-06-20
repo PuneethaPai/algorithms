3ef69b81cbd1f9f17eacd2deee3c0e0c5eb55271
Puneetha Pai <Puneetha_Pai@external.mckinsey.com>
2019-06-20 23:50:47 +0530

    Add union method and test symmentry property of the same

--- src/UnionFind.java
+++ src/UnionFind.java
@@ -1,11 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 class UnionFind {
-    private final int size;
+
+    private final List<List> state;
 
     UnionFind(int size) {
 
-        this.size = size;
+        this.state = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            this.state.add(Arrays.asList(i));
+        }
+    }
+
+    private UnionFind(List<List> state) {
+
+        this.state = state;
     }
 
     @Override
@@ -13,11 +26,21 @@ class UnionFind {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         UnionFind unionFind = (UnionFind) o;
-        return size == unionFind.size;
+        return state.equals(unionFind.state);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(size);
+        return Objects.hash(state);
+    }
+
+    UnionFind union(int source, int target) {
+        List<List> combined = state
+                .stream()
+                .filter(list -> list.contains(source) || list.contains(target))
+                .collect(Collectors.toList());
+        List<List> remaining = state.stream().filter(list -> !(list.contains(source) || list.contains(target))).collect(Collectors.toList());
+        combined.addAll(remaining);
+        return new UnionFind(combined);
     }
 }
--- test/UnionFindTest.java
+++ test/UnionFindTest.java
@@ -8,4 +8,11 @@ public class UnionFindTest {
     public void expect2UnionFindToBeEqual() {
         assertEquals(new UnionFind(5), new UnionFind(5));
     }
+
+    @Test
+    public void expectUnionToBeSymmetric() {
+        UnionFind unionFind1 = new UnionFind(3).union(1, 2);
+        UnionFind unionFind2 = new UnionFind(3).union(2, 1);
+        assertEquals(unionFind1, unionFind2);
+    }
 }
